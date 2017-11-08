84e195409fe946c22911ebebc223b2cc101ae3eb
SegFault42 <SegFault42@protonmail.com>
2017-11-08 21:03:25 +0100

    update

--- restore.sh
+++ restore.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+
 cp ./.zshrc ~/
 cp ./.vimrc ~/
+
+rm -fr ~/.config/polybar
 cp -fr ./polybar ~/.config/
+
+rm -fr ~/.config/i3
 cp -fr ./i3 ~/.config/
+
+rm -fr ~/.config/mps-youtube
 cp -fr ./mps-youtube ~/.config/
+
+rm -fr ~/.config/dconf/
 cp -fr ./dconf ~/.config/
+
+rm -fr ~/Pictures/*
 cp -fr ./Wallpapers/* ~/Pictures/
--- save.sh
+++ save.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+
 cp  ~/.zshrc .
 cp ~/.vimrc .
+
+rm -fr ./polybar
 cp -fr ~/.config/polybar .
+
+rm -fr ./i3
 cp -fr ~/.config/i3 .
+
+rm -fr ./mps-youtube
 cp -fr ~/.config/mps-youtube .
+
+rm -fr ./dconf
 cp -fr ~/.config/dconf .
+
+rm -fr ./Wallpapers/*
 cp -fr ~/Pictures/* ./Wallpapers/
