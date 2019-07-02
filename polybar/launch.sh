22ee730b89ffdf2e9a5d3f8668d3e74a3e4da0f5
SegFault42 <SegFault42@protonmail.com>
2019-07-02 13:59:01 +0200

    update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,5 +13,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
+
 # Launch
-polybar -r top
+#polybar -r top
