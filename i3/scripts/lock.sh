a7e83ae8e31925867f55b5be7220d440224b0bdb
SegFault42 <SegFault42@protonmail.com>
2019-08-15 22:33:22 +0200

    update sampler

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -8,3 +8,6 @@ convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 i3lock -u -i $TMPBG
 rm -rf $TMPBG
+/usr/bin/termite
+
+#dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Suspend" boolean:true
